e4270b31d7c1e2f91b82ac8995311c753867c542
Peter Reutemann <fracpete@gmail.com>
2025-09-17 16:00:23 +1200

    fixed has_finished() method

--- src/idc/video/reader/_from_video_file.py
+++ src/idc/video/reader/_from_video_file.py
@@ -232,7 +232,7 @@ class VideoFileReader(Reader, PlaceholderSupporter, DataTypeSupporter):
         :return: True if finished
         :rtype: bool
         """
-        return len(self._inputs) == 0
+        return (self._inputs is not None) and len(self._inputs) == 0
 
     def finalize(self):
         """
